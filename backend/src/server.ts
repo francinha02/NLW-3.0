72432b41328a6484a318994b507b14c7b91973ce
David <davidemanuel.cf@gmail.com>
2020-10-17 09:08:41 -0300

    Day 1 and Day 2

---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import "./database/connections";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (request, response) => {
+  response.json({ message: "Hello World" });
+});
+
+app.listen(3333);
